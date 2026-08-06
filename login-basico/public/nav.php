bb57edc4bcf1659516f6317beeef0da3228bf5d6
Giordana Rubin <giordana1g@gmail.com>
2026-08-06 12:46:23 -0300

    Finalização login básico

--- login-basico/public/nav.php
+++ login-basico/public/nav.php
@@ -4,13 +4,13 @@ defined('CONTROL') or die('Acesso negado.');
 
 <hr>
 <span>Usuário: <strong><?php echo $_SESSION['usuario'] ?></strong></span>
-<br>
 <span><a href="index.php?rota=logout">Sair</a></span>
 <hr>
 
 <nav>
     <a href="?rota=home">Home</a>
-    <a href="?rota=home">Home</a>
-    <a href="?rota=home">Home</a>
-    <a href="?rota=home">Home</a>
+    <a href="?rota=page1">Página 1</a>
+    <a href="?rota=page2">Página 2</a>
+    <a href="?rota=page3">Página 3</a>
+    <a href="index.php?rota=logout">Sair</a>
 </nav>
--- login-basico/public/page3.php
+++ login-basico/public/page3.php
@@ -4,18 +4,16 @@ defined('CONTROL') or die('Acesso negado.');
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplicação</title>
+    <title>Página 3</title>
 </head>
-
 <body>
     <?php require 'nav.php' ?>
 
-    <h3>Bem vindo!</h3>
-    [Conteúdo]
+    <h3>Página 3</h3>
+    <hr>
+    [conteúdo]
 </body>
-
 </html>
